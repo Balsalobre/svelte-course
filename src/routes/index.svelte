73999e445a123243e1bcbdda4088cd1cc6e66c76
balsalobre <carlosocb@gmail.com>
2022-08-11 12:08:43 +0200

    Installing sass

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
+<div class="title">
+  <h1>Welcome to SvelteKit</h1>
+</div>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+  .title {
+    h1 {
+      color: lightcoral;
+    }
+  }
+</style>
